80dfc01ba52ecd85075204667f4ba617eab27e21
wa1zzz <korobitsyn.nick@yandex.ru>
2025-09-12 09:49:27 +0300

    Create 123.py

---
+++ 123.py
@@ -0,0 +1,14 @@
+# Сохраним строку в переменной.
+str_1 = 'AaBbCcDd'
+# Выводим на экран требуемые строки.
+print('str_1[::2] ->', str_1[::2], end='\n\n')
+# Смещаем вывод на единицу.
+print('str_1[1::2]->', str_1[1::2])
+
+
+#создание стека
+stack =
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print(stack)
