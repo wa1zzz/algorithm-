92892d4a6e293b831067d4abf2d67e7d72cd84c6
wa1zzz <korobitsyn.nick@yandex.ru>
2025-09-15 14:19:53 +0300

    Create 123.java

---
+++ 123.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String str_1 = "AaBbCcDd";
+        StringBuilder uppercaseStr = new StringBuilder();
+        StringBuilder lowercaseStr = new StringBuilder();
+        
